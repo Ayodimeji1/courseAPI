32ba1f22726a734ea10430dfce47b075fc6e069b
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-09-19 00:51:53 +0100

    updating views to mixins

--- course/views.py
+++ course/views.py
@@ -10,42 +10,71 @@ from rest_framework.views import APIView
 # Create your views here.
 
 
-class CourseList(APIView):
-
-    def get(self,request):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data)
-
-    def post(self,request):
-        serializer = CourseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CourseList(mixins.CreateModelMixin, mixins.ListModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
 
-class CourseDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Course.objects.get(pk=pk)
-        except Course.DoesNotExist:
-            raise Http404
+    def get(self, request):
+        return self.List(request)
+
+    def post(self,requery):
+        return self.create()
+
+
+class CoureDetail(mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
     def get(self,request,pk):
-        course = self.get_object(pk)
-        serializer = CourseSerializer(course)
-        return Response(serializer.data)
+        return self.retrieve(request,pk)
 
     def put(self,request,pk):
-        course = self.get_object(pk)
-        serializer = CourseSerializer(course,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.Update(request,pk)
 
     def delete(self,request,pk):
-        course = self.get_object(pk)
-        course.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return self.destroy(request,pk)
+
+
+
+
+
+
+
+#     def get(self,request):
+#         courses = Course.objects.all()
+#         serializer = CourseSerializer(courses, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self,request):
+#         serializer = CourseSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class CourseDetail(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Course.objects.get(pk=pk)
+#         except Course.DoesNotExist:
+#             raise Http404
+#
+#     def get(self,request,pk):
+#         course = self.get_object(pk)
+#         serializer = CourseSerializer(course)
+#         return Response(serializer.data)
+#
+#     def put(self,request,pk):
+#         course = self.get_object(pk)
+#         serializer = CourseSerializer(course,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self,request,pk):
+#         course = self.get_object(pk)
+#         course.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
